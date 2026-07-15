2c0789f10f64b1ea0f5f955cb21e18004302b784
Sri <srinukaram121@gmail.com>
2026-07-15 13:40:29 +0530

    LeetCode 240

---
+++ src/Arrays/Search_2D_matrix_240.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+public class Search_2D_matrix_240 {
+
+	public static void main(String[] args) {
+		int[][] arr= {
+				      {1,4,7,11,15},
+				      {2,5,8,12,19},
+				      {3,6,9,16,22},
+				      {10,13,14,17,24},
+				      {18,21,23,26,30}
+				     };
+		int target = 5;
+		System.out.println(Search(arr,target));
+
+	}
+
+	 static int Search(int[][] arr, int target) {
+		 int row=0;
+		 int col=arr[0].length-1;
+		 while(row <arr.length && col >=0) {
+			 if(target==arr[row][col]) {
+				 return target;
+			 }
+			//  ------Key points--------
+		        // left --->  Smaller elements
+		        //right --->  Bigger elements
+		        //up    --->  Smaller elements
+		        //down  --->  Bigger elements 
+			 if(target>arr[row][col]) {
+				 row++;
+			 }else {
+				 col--;
+			 }
+			 
+		 }
+		return -1;
+	}
+
+}
